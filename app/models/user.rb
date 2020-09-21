74096f38d71614e147b1cebb89660b0c32c51ac3
Quinn <quinnvoker@gmail.com>
2020-09-21 23:32:33 +0000

    added authenticate_with_credentials to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  def self.authenticate_with_credentials(email, password)
+    user = Users.find_by(email: email)
+    if user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
