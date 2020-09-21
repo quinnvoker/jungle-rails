3537204dbe9ff159e681d73dfa33a15332090c93
Quinn <quinnvoker@gmail.com>
2020-09-21 23:13:38 +0000

    added all password validity tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :password, presence: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 3 }
   validates :password_confirmation, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,5 +21,19 @@ RSpec.describe User, type: :model do
 
       expect(user.errors.full_messages).to include("Password confirmation can't be blank")
     end
+
+    it 'should fail to save if password and confirmation do not match' do
+      user = User.new(first_name: 'John', last_name: 'Smith', email: 'john@smith.com', password: 'jonny', password_confirmation: 'jonny5')
+      user.save
+
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'should fail to save if password is shorter than 3 characters' do
+      user = User.new(first_name: 'John', last_name: 'Smith', email: 'john@smith.com', password: 'jo', password_confirmation: 'jo')
+      user.save
+
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 3 characters)")
+    end
   end
 end
