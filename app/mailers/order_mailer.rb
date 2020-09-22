d8123d50a42279909cd256a6164239c7bbc89702
Quinn <quinnvoker@gmail.com>
2020-09-22 22:29:57 +0000

    set up mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,8 @@
 class OrderMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def complete_email(order)
+    @order = order
+    mail(to: @order.email, subject: "Confirmation of Order ##{@order.id}")
+  end
 end
