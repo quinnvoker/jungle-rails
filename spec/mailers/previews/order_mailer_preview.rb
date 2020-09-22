bb167cd918e2294baf58b65ead4358b51a8a9673
Quinn <quinnvoker@gmail.com>
2020-09-22 23:21:59 +0000

    formatted order email

--- spec/mailers/previews/order_mailer_preview.rb
+++ spec/mailers/previews/order_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
-
+  def confirmation_email
+    OrderMailer.confirmation_email(Order.last)
+  end
 end
